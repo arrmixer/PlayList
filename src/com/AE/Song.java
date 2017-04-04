57b37dbfd3b838959c4b5e5751b837b26e08ffc6
Angel Rodriguez <arrmixer@gmail.com>
2017-04-04 12:08:21 -0400

    Initial commit

---
+++ src/com/AE/Song.java
@@ -0,0 +1,27 @@
+package com.AE;
+
+/**
+ * Created by Angel on 12/9/16.
+ */
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+}
